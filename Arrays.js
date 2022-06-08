5f60ce16a9f735ccd8df051024da1b872fd43ae2
Bencee Yang <byang@icstars.org>
2022-06-08 11:47:16 -0500

    JavaScript Arrays + functions

---
+++ Arrays.js
@@ -0,0 +1,32 @@
+// let secretMessage = ['Learning', 'is', 'not', 'about', 'what', 'you',
+// 'get', 'easily', 'the', 'first', 'time,', 'it', 'is', 'about', 'what', 'you',
+// 'can', 'figure', 'out.', '-2015,', 'Chris', 'Pine,', 'Learn',
+// 'JavaScript']
+
+// let popped = secretMessage.pop()
+
+// NOTE //Console.log(secretMessage)
+// console.log(secretMessage.push('To','Program'))
+// NOTE //console.log(secretMessage)
+// NOTE //secretMessage['easily'] = 'right'
+
+// secretMessage.splice(7,1,"right")
+// NOTE //console.log(secretMessage)
+// secretMessage.shift()
+// secretMessage.unshift('Programming')
+// secretMessage.splice(6,5,'know')
+// secretMessage.join(popped)
+// secretMessage.push(popped)
+// onslotchange.log(secretMessage.join(``))
+
+NOTE //this is a function to return two numbers
+// function number(a,b){
+//     return (1,2);
+// }
+// console.log(),number(a,b)
+
+NOTE //this is the same function as above but console.log is inside the function
+function number(a,b){
+    return(console.log(a+b))
+}
+number(1,2)
---
+++ Task.js
@@ -0,0 +1,11 @@
+// Create a function that takes a number as its only argument and returns true if
+// it's less than or equal to zero, otherwise return false
+function morning(a){
+    if(a <= 0){
+        return true
+    }
+    else{
+        return false
+    }
+}
+console.log(morning(2))
